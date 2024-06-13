cce320f3ecbb118d45f0c7b40adf6b9d129d2965
codeshaper2077 <dartWayne1998android@gmail.com>
2024-06-12 18:32:54 -0700

    Added Coordinator.swift

---
+++ CoordinatorApp/Coordinator.swift
@@ -0,0 +1,25 @@
+//
+//  Coordinator.swift
+//  CoordinatorApp
+//
+//  Created by user on 12.06.2024.
+//
+
+import UIKit
+
+enum Event {
+    case buttonTapped
+}
+
+protocol Coordinator {
+    var navigationController: UINavigationController? { get set }
+    
+    func eventOccured(with type: Event) //
+    
+    func start() // the call after the start of the application or coordinator
+    
+}
+
+protocol Coordinating {
+    var coordinator: Coordinator? { get set }
+}
