49f0720ae17ccaaee34f17489fca03a8f6eac26c
codeshaper2077 <dartWayne1998android@gmail.com>
2024-06-12 18:57:35 -0700

    Added MainCoordinator.swift

---
+++ CoordinatorApp/CoordinatorApp/MainCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  MainCoordinator.swift
+//  CoordinatorApp
+//
+//  Created by user on 12.06.2024.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var navigationController: UINavigationController?
+    
+    func eventOccured(with type: Event) {
+        
+    }
+    
+    func start() {
+        
+    }
+    
+    
+}
